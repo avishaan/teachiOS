88134a3312c1deef55580507d2b902ebcfaeffa9
Avishaan <avishaan@codeHatcher.com>
2015-03-20 18:08:04 -0400

    add view controllers for updating view

---
+++ LemonadeStand/Price.swift
@@ -0,0 +1,14 @@
+//
+//  Price.swift
+//  LemonadeStand
+//
+//  Created by Brown Magic on 3/20/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import Foundation
+
+struct Price {
+  let lemon = 2
+  let iceCube = 1
+}
--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -18,6 +18,13 @@ class ViewController: UIViewController {
   @IBOutlet weak var lemonMixCount: UILabel!
   @IBOutlet weak var iceCubeMixCount: UILabel!
   
+  var supplies = Supplies(aMoney: 10, aLemons: 1, aIceCubes: 1)
+  let price = Price()
+  
+  var lemonsToPurchase = 0
+  var iceCubesToPurchase = 0
+  var lemonsToMix = 0
+  var iceCubesToMix = 0
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -47,6 +54,25 @@ class ViewController: UIViewController {
   @IBAction func unmixIceCubeButtonPressed(sender: UIButton) {
   }
   @IBAction func startDayButtonPressed(sender: UIButton) {
+  }
+  
+  // Helper Functions
+  func updateMainView () {
+    moneySupplyCount.text = "$\(supplies.money)"
+    lemonSupplyCount.text = "\(supplies.lemons) Lemons"
+    iceCubeSupplyCount.text = "\(supplies.iceCubes) Ice Cubes"
+    
+    lemonPurchaseCount.text = "\(lemonsToPurchase)"
+    iceCubePurchaseCount.text = "\(iceCubesToPurchase)"
+    
+    lemonMixCount.text = "\(lemonsToMix)"
+    iceCubeMixCount.text = "\(iceCubesToMix)"
+  }
+  
+  func showAlertWithText (header:String = "Warning", message:String) {
+    var alert = UIAlertController(title: header, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+    self.presentViewController(alert, animated: true, completion: nil)
   }
   
 
